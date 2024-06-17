558e38cb1992823cdee4fe0458dcad0472eff029
root-goksenin <Goksenin.yuksel@outlook.com>
2024-06-17 18:01:50 +0200

    Add dev dependencies

--- builds_and_runs.py
+++ builds_and_runs.py
@@ -1,10 +1,12 @@
 import pytest
-import rgpl
 import rgpl.hard_negative_miner
 import rgpl.lightning
 import rgpl.query_models
 import rgpl.trainer
 import rgpl.utils
 
+
+def test_dummy():
+    pass
 
 
